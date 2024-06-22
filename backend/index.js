675381c3955c896c1ee4cfbf9a6cd9b6e816655a
Mridula Chowdhary <mridulachowdhary21@gmail.com>
2024-06-22 11:18:57 +0530

    Added basic backend setup

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express= require("express");
+const app=express();
+
+app.use(express.json());
+
+app.get('/todo',function(req,res){
+
+})
+app.post('/todos',function(req,res){
+
+})
+
+app.put('/completed',function(req,res){
+
+})
+
+app.listen(3000);
